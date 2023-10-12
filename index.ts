71101c2033091671f6f62803c26cbb5d3152bf92
Numan Abubakar <bhalonoman@gmail.com>
2023-10-13 01:56:39 +0500

    install chalk

--- index.ts
+++ index.ts
@@ -1,15 +1,4 @@
-// Develop a TS program that show the working of an ATM machine such
-
 import inquirer, { Answers } from "inquirer";
-
-//  - User should login by entering his/her unique account number and a secret PIN
-// - User can check account balance
-// - User can withdraw money
-// - User can check his/her previous transactions in the current login
-// - User should be asked at the end of each function that if he/she wants to end transections or perform any other transections if the user choose more transections he/she must be shown all the options again and he / she can perform any transections.
-
-// In the current login means if user logged in and performed 5 transections he/she have an option to check these 5 transections
-// If he/she choosed to end the transections the data should be removed.
 
 interface UserType {
   accNumber: number;
@@ -208,7 +197,9 @@ function askTransaction() {
     if (answer.selected == "Yes") {
       Home();
     } else {
-      console.log("Thank Your For Using Our Bank ");
+      console.log(
+        "Thank Your For Using Our Bank and Hope We will See you Again :)"
+      );
     }
   });
 }
